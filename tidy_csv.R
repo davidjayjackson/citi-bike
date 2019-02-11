8063b4778fe67db3503c8366bf9eb509343b3636
davidjayjackson <davidjayjackson@gmail.com>
2019-02-11 13:02:49 -0500

    tweak a couplr of settings best result for csv data 88 sec.

--- tidy_csv.R
+++ tidy_csv.R
@@ -5,8 +5,15 @@ library(RMySQL)
 rm(cbike)
 rm(ptm)
 rm(ptm1)
-ptm <-proc.time()
-cbike <-dir("data",full.names=T) %>% map_df(read_csv)
+
+rm(cbike)
+rm(ptm)
+rm(ptm1)
+rm(ptn2)
+# library(data.table)
+ptm1 <-proc.time()
+cbike <-dir("data",full.names=T) %>% map_df(data.table::fread)
+proc.time() - ptm1
 # Rename columns
 names(cbike) <-c("tripduration","starttime","stoptime","startstationid",
                  "startstationname","startstationlatitude",
@@ -14,17 +21,17 @@ names(cbike) <-c("tripduration","starttime","stoptime","startstationid",
                  "endstationname","endstationlatitude","
                  endstationlongitude","bikeid","usertype",
                  "birthyear","gender")
-proc.time() - ptm
+
 # SQLite stuff
 # Change Date to text before importing into SQLite
 cbike$starttime <- as.character(cbike$starttime)
 cbike$stoptime <- as.character(cbike$stoptime)
-ptm1 <-proc.time()
+ptm2 <-proc.time()
 db <- dbConnect(SQLite(),dbname="city_bike.sqlite3")
 dbWriteTable(db,"cbike",cbike,row.names=FALSE,overwrite=TRUE)
-dbListTables(db)
-dbDisconnect(db)
-proc.time() -ptm1
+# dbListTables(db)
+# dbDisconnect(db)
+proc.time() -ptm2
 #
 dbSendStatement(db, "create index duration on cbike(tripduration)")
 dbSendStatement(db, "create index startstation on cbike(startstationid)")
@@ -39,7 +46,7 @@ rm(ptm1)
 # Create table an
 mydb <- dbConnect(MySQL(),user='root',password='dJj12345',dbname="test",
 host='localhost')
-ptm2 <-proc.time
+ptm4 <-proc.time
 dbWriteTable(mydb, "cbike", cbike, row.names = FALSE)
 dbListTables(mydb)
-proc.time() - pmt2
+proc.time() - pmt4
